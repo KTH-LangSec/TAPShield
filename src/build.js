0c264b4594460be2c66b36037b9207afbda200a3
Rof13thfloor <mojtaba.moazen.a@gmail.com>
2023-09-25 17:12:06 +0200

    5 flow are working well

--- src/build.js
+++ src/build.js
@@ -43,7 +43,7 @@ export async function main() {
     minify: config.minify,
     sourcemap: config.sourcemap,
     platform: "node",
-    target: "node14",
+    target: "es6",
     outfile: path.join(DISTDIR, "index.js"),
     plugins: [
       nullPackage([
