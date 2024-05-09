3a2f91c8aa0242deef89af3afc2cb610967a865b
Rof13thfloor <mojtaba.moazen.a@gmail.com>
2024-05-09 12:57:33 +0200

    docker changed

--- start-docker-ubuntu.sh
+++ start-docker-ubuntu.sh
@@ -8,3 +8,9 @@ docker start dcap_server
 # Once the container is running we can use it to execute the pf_crypt binary in it for encrypting all files in user-dir
 
 docker exec dcap_server bash -c "cd node-red-sgx && sh ./encrypt-user-dir.sh"
+
+
+
+# run this on the server 
+
+RA_TLS_ALLOW_DEBUG_ENCLAVE_INSECURE=1 RA_TLS_ALLOW_OUTDATED_TCB_INSECURE=1 RA_TLS_ALLOW_HW_CONFIG_NEEDED=1 RA_TLS_ALLOW_SW_HARDENING_NEEDED=1 ./server_dcap wrap_key
